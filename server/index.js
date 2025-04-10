3b38cfb8d99ed3e713aac6734e44a4982ef07571
DMGoose <owdran@gmail.com>
2025-04-10 15:13:36 +0200

    ingoreEnv

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use("/AI", AIRoutes);
+app.use("/api", AIRoutes);
 
 const server = app.listen(process.env.PORT, ()=> {
     console.log('Server started on port '+ process.env.PORT);

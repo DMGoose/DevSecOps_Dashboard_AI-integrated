fb116718f87cc8859e648c2fcfedc1517f357d99
DMGoose <owdran@gmail.com>
2025-04-10 15:15:34 +0200

    refactor2.0

--- src/style/color.jsx
+++
@@ -1,9 +0,0 @@
-const RISK_COLORS = {
-    'High': '#ff3b3b',
-    'Medium': '#ffb700',
-    'Low': '#faff00',
-    'Info': '#007bff',
-    'False +': '#00b050'
-};
-
-export default RISK_COLORS;
--- src/utils/APIRoutes.js
+++
@@ -1,3 +0,0 @@
-// API Routes
-export const host = "http://localhost:5000";
-export const getSuggestions = `${host}/AI/getSuggestions`
